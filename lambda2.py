9294bbc002fbbb421188882f5d1d4567de5bbe94
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2024-01-25 14:49:53 +0530

    update function: squarefunction

--- lambda2.py
+++ lambda2.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     number = int(event.get("number", 0))
-    result = number ** 2
+    result = number ** 3
     return {
         "statusCode": 200,
         "body": f"The square of {number} is {result}"
