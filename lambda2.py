477c66b2ace386bf59ebeaf962405e1f33b38f5f
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2024-01-25 14:41:52 +0530

    Update lambda2.py

--- lambda2.py
+++ lambda2.py
@@ -1,1 +1,8 @@
+def lambda_handler(event, context):
+    number = int(event.get("number", 0))
+    result = number ** 2
+    return {
+        "statusCode": 200,
+        "body": f"The square of {number} is {result}"
+    }
 
