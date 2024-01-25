72b189a1e71c866d01c6090568f7aa9937687d24
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2024-01-25 14:41:33 +0530

    Update lambda1.py

--- lambda1.py
+++ lambda1.py
@@ -1,1 +1,6 @@
-example df
+def lambda_handler(event, context):
+    message = "Lambda1 says: Hello, " + event.get("name", "Guest")
+    return {
+        "statusCode": 200,
+        "body": message
+    }
